6f7bc9f21b735d1491e225b85fad087dfae41cda
wzc520pyfm <1528857653@qq.com>
2022-12-14 10:45:36 +0800

    fix：修复全局加载动画不居中的问题

--- src/components/common/ElementProvider.vue
+++ src/components/common/ElementProvider.vue
@@ -8,6 +8,8 @@ import "nprogress/nprogress.css"; // progress bar style
 
 defineOptions({ name: "ElementProvider" });
 
+// TODO: 这是一个老但稳定的loadingBar, 暂时先使用它, 后续再重写
+// see: https://www.npmjs.com/package/nprogress
 NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
 /** 挂载常用组件至window, 以便在钩子函数和请求函数里面调用 */
