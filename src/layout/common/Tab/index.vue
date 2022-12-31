e43b8a1c40fe066802ad094af12a6c7d100f309c
wzc520pyfm <1528857653@qq.com>
2022-12-31 18:56:45 +0800

    feat(projects): 增加tab栏溢出后滚动

--- src/layout/common/Tab/index.vue
+++ src/layout/common/Tab/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="global-tab flex-1-hidden h-full flex justify-between dark:bg-dark">
-    <tab-detail />
+    <div ref="bsWrapper" class="flex-1-hidden h-full">
+      <better-scroll ref="bsScroll" :options="{ scrollX: true, scrollY: false, click: true }">
+        <tab-detail @scroll="handleScroll" />
+      </better-scroll>
+    </div>
     <ReloadButton />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useElementBounding } from "@vueuse/core";
 import { useTabStore } from "@/stores";
 import { TabDetail, ReloadButton } from "./components";
 
@@ -13,6 +18,22 @@ defineOptions({ name: "GlobalTab" });
 
 const route = useRoute();
 const tab = useTabStore();
+
+const bsWrapper = ref<HTMLElement>();
+const { width: bsWrapperWidth, left: bsWrapperLeft } = useElementBounding(bsWrapper);
+
+const bsScroll = ref<Expose.BetterScroll>();
+
+function handleScroll(clientX: number) {
+  const currentX = clientX - bsWrapperLeft.value;
+  const deltaX = currentX - bsWrapperWidth.value / 2;
+  if (bsScroll.value) {
+    const { maxScrollX, x: leftX } = bsScroll.value.instance;
+    const rightX = maxScrollX - leftX;
+    const update = deltaX > 0 ? Math.max(-deltaX, rightX) : Math.min(-deltaX, -leftX);
+    bsScroll.value?.instance.scrollBy(update, 0, 300);
+  }
+}
 
 function init() {
   tab.initTabStore(route);
