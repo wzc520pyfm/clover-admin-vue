41899e68066e6386420987d231337d780509b6fc
wzc520pyfm <69044080+wzc520pyfm@users.noreply.github.com>
2023-01-01 19:33:52 +0800

    feat(plugins): 引入swiper, 增加swiper示例 (#5)
    * build(projects): 引入swiper
    
    * feat(components): 封装link组件
    
    * feat(views): 增加swiper示例

---
+++ src/views/plugin/swiper/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="w-full">
+    <el-card class="w-full">
+      <el-space direction="vertical" alignment="normal">
+        <github-link link="https://github.com/nolimits4web/swiper" />
+        <web-site-link label="vue3版文档地址：" link="https://swiperjs.com/vue" />
+        <web-site-link label="插件demo地址：" link="https://swiperjs.com/demos" />
+      </el-space>
+      <el-space direction="vertical" alignment="normal" class="w-full">
+        <div class="w-full" v-for="item in swiperExample" :key="item.id">
+          <h3 class="py-24px text-24px font-bold">{{ item.label }}</h3>
+          <swiper v-bind="item.options">
+            <swiper-slide v-for="i in 5" :key="i">
+              <div class="flex-center h-240px border-1px border-#999 text-18px font-bold">
+                Slide{{ i }}
+              </div>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </el-space>
+    </el-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import type { SwiperOptions } from "swiper";
+
+type SwiperExampleOptions = Pick<
+  SwiperOptions,
+  | "navigation"
+  | "pagination"
+  | "scrollbar"
+  | "slidesPerView"
+  | "slidesPerGroup"
+  | "spaceBetween"
+  | "direction"
+  | "loop"
+  | "loopFillGroupWithBlank"
+  | "autoplay"
+>;
+
+interface SwiperExample {
+  id: number;
+  label: string;
+  options: Partial<SwiperExampleOptions>;
+}
+
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
+const swiperExample: SwiperExample[] = [
+  { id: 0, label: "Default", options: {} },
+  {
+    id: 1,
+    label: "Navigation",
+    options: { navigation: true },
+  },
+  {
+    id: 2,
+    label: "Pagination",
+    options: { pagination: true },
+  },
+  {
+    id: 3,
+    label: "Pagination dynamic",
+    options: {
+      pagination: { dynamicBullets: true },
+    },
+  },
+  {
+    id: 4,
+    label: "Pagination progress",
+    options: {
+      navigation: true,
+      pagination: {
+        type: "progressbar",
+      },
+    },
+  },
+  {
+    id: 6,
+    label: "Slides per view",
+    options: {
+      pagination: {
+        clickable: true,
+      },
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  {
+    id: 7,
+    label: "Infinite loop",
+    options: {
+      navigation: true,
+      pagination: {
+        clickable: true,
+      },
+      loop: true,
+    },
+  },
+  {
+    id: 8,
+    label: "Autoplay loop",
+    options: {
+      autoplay: {
+        delay: 1000, // 切换时延
+        disableOnInteraction: false, // 禁用交互, 在用户交互后继续播放
+        pauseOnMouseEnter: true, // 鼠标悬停时停止播放
+      },
+      loop: true,
+    },
+  },
+];
+</script>
+
+<style lang="less" scoped>
+.el-card {
+  #ep.el-card-rounded();
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig(({ mode }) => {
     },
     // 依赖优化选项
     optimizeDeps: {
-      include: [], // 需要强制预构建的包
+      include: ["swiper", "swiper/vue"], // 需要强制预构建的包
     },
     // 构建选项
     build: {
