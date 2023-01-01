38a1553139a8816ec145c5941a8aa95061532c3d
wzc520pyfm <1528857653@qq.com>
2023-01-01 23:43:40 +0800

    feat(views): 更新高德地图跟随暗黑模式切换style

--- src/views/plugin/map/components/GaodeMap.vue
+++ src/views/plugin/map/components/GaodeMap.vue
@@ -5,21 +5,40 @@
 <script lang="ts" setup>
 import { useScriptTag } from "@vueuse/core";
 import { GAODE_MAP_SDK_URL } from "@/config";
+import { useThemeStore } from "@/stores";
 
+const MapStyle = ["amap://styles/normal", "amap://styles/darkblue"];
+
+const theme = useThemeStore();
 const { load } = useScriptTag(GAODE_MAP_SDK_URL);
+let map: AMap.Map;
 
 const domRef = ref<HTMLDivElement>();
 
 async function renderMap() {
   if (!domRef.value) return;
   await load(true);
-  const map = new AMap.Map(domRef.value, {
+  map = new AMap.Map(domRef.value, {
     zoom: 11,
     center: [116.397428, 39.90923],
     viewMode: "3D",
+    mapStyle: theme.isDark.value ? MapStyle[1] : MapStyle[0],
   });
   map.getCenter();
 }
+
+function setMapStyle(map: AMap.Map) {
+  if (theme.isDark.value) {
+    map.setMapStyle(MapStyle[1]);
+  } else {
+    map.setMapStyle(MapStyle[0]);
+  }
+}
+
+watch(
+  () => theme.isDark.value,
+  () => void setMapStyle(map)
+);
 
 onMounted(() => {
   renderMap();
