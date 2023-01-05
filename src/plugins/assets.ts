53efcbbd0be59372918761dd906031c7c9ddc7a6
wzc520pyfm <69044080+wzc520pyfm@users.noreply.github.com>
2023-01-05 22:01:44 +0800

    refactor(components): [progress] 调整progress样式文件的导入位置 (#19)

--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -3,6 +3,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "vue3-lottie/dist/style.css";
+import "nprogress/nprogress.css"; // progress bar style
 import "element-plus/theme-chalk/dark/css-vars.css";
 import "../styles/css/main.css";
 
