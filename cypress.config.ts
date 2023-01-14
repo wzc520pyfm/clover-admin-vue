3d360e12e492f2dcff47d9ce99510b8eb37bfef5
wzc520pyfm <69044080+wzc520pyfm@users.noreply.github.com>
2023-01-14 20:16:19 +0800

    test(project): :white_check_mark: 配置cypress (#48)
    增加cypress案例
    
    配置cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "1zvgo9",
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
