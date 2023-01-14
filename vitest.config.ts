318892082fc14ae481c7efc811d9169fbddb421b
wzc520pyfm <1528857653@qq.com>
2023-01-14 17:53:17 +0800

    chore(project): :hammer: 配置vitest
    配置vitest
    
    增加vscode插件vitest

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitest/config";
+import Vue from "@vitejs/plugin-vue";
+import VueJsx from "@vitejs/plugin-vue-jsx";
+import VueMacros from "unplugin-vue-macros/vite";
+
+export default defineConfig({
+  plugins: [
+    VueMacros({
+      setupComponent: false,
+      setupSFC: false,
+      plugins: {
+        vue: Vue(),
+        vueJsx: VueJsx(),
+      },
+    }),
+  ],
+  optimizeDeps: {
+    disabled: true,
+  },
+  test: {
+    clearMocks: true,
+    environment: "jsdom",
+    transformMode: {
+      web: [/\.[jt]sx$/],
+    },
+  },
+});
