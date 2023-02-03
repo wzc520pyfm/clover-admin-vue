bee3d272945031da4eeb203e78e3df3d81a046ef
wzc520pyfm <1528857653@qq.com>
2023-02-03 18:49:09 +0800

    feat(views): :sparkles: add wangEditor and sample

---
+++ src/views/plugin/editor/quill/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full pb-5">
+    <el-card class="w-full" header="富文本插件">
+      <github-link link="https://github.com/wangeditor-team/wangEditor" />
+      <div ref="domRef" mt-10px />
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import WangEditor from "wangeditor";
+
+defineOptions({ name: "PluginEditorQuill" });
+
+let editor = $ref<WangEditor>();
+const domRef = $ref<HTMLElement>();
+
+function renderWangEditor() {
+  editor = new WangEditor(domRef);
+  setEditorConfig();
+  editor.create();
+}
+
+function setEditorConfig() {
+  if (editor?.config?.zIndex) {
+    editor.config.zIndex = 10;
+  }
+}
+
+onMounted(() => renderWangEditor());
+</script>
+
+<style lang="less" scoped>
+.el-card {
+  #ep.el-card-rounded();
+}
+</style>
