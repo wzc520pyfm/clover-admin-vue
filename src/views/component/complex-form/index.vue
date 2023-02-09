3f706cfd5c86a5b1d613f11cc4347517602390a8
wzc520pyfm <1528857653@qq.com>
2023-02-09 19:37:58 +0800

    feat(views): :sparkles: add chian form in complex-from page

---
+++ src/views/component/complex-form/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="min-h-full">
+    <el-card class="h-full" header="复杂表单示例">
+      <el-space w-full direction="vertical" size="large" fill>
+        <BaseValidatorForm />
+        <CustomValidatorForm />
+        <ResponsibilityValidatorForm />
+      </el-space>
+    </el-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BaseValidatorForm, CustomValidatorForm, ResponsibilityValidatorForm } from "./components";
+
+defineOptions({ name: "ComponentComplexForm" });
+</script>
+
+<style lang="less" scoped>
+.el-card {
+  #ep.el-card-rounded();
+}
+</style>
